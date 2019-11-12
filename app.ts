c059eca3f90e9f40f0988b3b6026e6bf6513efc3
Ashkan Ashtiani <ashcan@3dln.com>
2019-11-12 08:29:18 +0330

    Classes vs. Interfaces in typescript - Second Post

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-interface Person {     
-    firstName: string;     
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleName: string, public lastName: string){
+        this.fullName = `${firstName} ${middleName} ${lastName}`;
+    }
+}
+interface Person {
+    firstName: string;
     lastName: string;
 }
 function sayHelloTo(person:Person) {
-    return (`hello ${person.firstName} ${person.lastName}`);
-}
-let user = { firstName: "Ashkan", lastName: "Ashtiani" };
-console.log(sayHelloTo(user));
+     return (`hello ${person.firstName} ${person.lastName}`);
+} 
+let student = new Student("Ashkan", "", "Ashtiani"); 
+console.log(sayHelloTo(student));
